3f52118888eb4467e067bfb229a34ae78a09ed5c
rafmando <rmarqdev@gmail.com>
2026-02-10 13:23:31 +0000

    feat: add DynamoDB tables for users, routes, and maps

--- backend/lib/route-bot-backend-stack.ts
+++ backend/lib/route-bot-backend-stack.ts
@@ -1,16 +1,26 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const usersTable = new dynamodb.TableV2(this, 'RouteBot-UsersTable', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING, },
+      tableName: 'RouteBot-Users'
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const routesTable = new dynamodb.TableV2(this, 'RouteBot-RoutesTable', {
+      partitionKey: { name: 'userId', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'createdAt', type: dynamodb.AttributeType.STRING },
+      tableName: 'RouteBot-Routes'
+    });
+
+    const mapTable = new dynamodb.TableV2(this, 'RouteBot-MapsTable', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      tableName: 'RouteBot-Maps'
+    });
   }
 }
